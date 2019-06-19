d817015292190ed5f9d792a0b4d0eb64b67e3324
Dhyanee Bhatt <dhyanee.bhatt@gmail.com>
2019-06-19 11:01:21 -0400

    commit

--- MiniGames/GameScene.swift
+++ MiniGames/GameScene.swift
@@ -9,14 +9,33 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene , SKPhysicsContactDelegate {
     
     var nextLevelButton:SKLabelNode!
     
+    // sprites
+ 
+    var lemmingArray:[SKNode] = []
+  
     
     override func didMove(to view: SKView) {
         print("This is level 1")
         self.nextLevelButton = self.childNode(withName: "nextLevelButton") as! SKLabelNode
+        
+        // Required for SKPhysicsContactDelegate
+        self.physicsWorld.contactDelegate = self
+        
+        // make array of lemming
+        let l1 = self.childNode(withName: "lemming1")
+        let l2 = self.childNode(withName: "lemming2")
+        let l3 = self.childNode(withName: "lemming3")
+        let l4 = self.childNode(withName: "lemming4")
+        self.lemmingArray.append(e1!)
+        self.lemmingArray.append(e2!)
+        self.lemmingArray.append(e3!)
+        self.lemmingArray.append(e4!)
+    
+    
     }
     
     override func update(_ currentTime: TimeInterval) {
